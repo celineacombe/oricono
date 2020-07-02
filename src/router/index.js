2a303a3204e23cefa81445f1e1985e0df5f58024
Céline Combe <celine.a.combe@gmail.com>
2020-07-02 20:33:14 +0200

    New route and new view for basket

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+//import Basket from '../views/Basket.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
+const routes = [
   {
     path: '/',
     name: 'Home',
@@ -17,6 +18,14 @@ Vue.use(VueRouter)
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/basket',
+    name: 'Basket',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/Basket.vue')
   }
 ]
 
---
+++ src/views/Basket.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <section class="basket">
+    <header>
+      <h1>Votre panier</h1>
+    </header>
+    <div class="main">
+      <ul class="basket-contents">
+        <li class="basket-article">Liste des articles du panier (à récupérer dans le component "x")</li>
+      </ul>
+    </div>
+    <div class="form">
+      <form action class="contact">
+        Vos coordonnées
+        <br />
+        <label for="firstName">Prénom :</label>
+        <input type="text" id="firstName" name="firstName" />
+        <br />
+        <label for="lastName">Nom :</label>
+        <input type="text" id="lastName" name="lastName" />
+        <br />
+        <label for="adress">Adresse :</label>
+        <input type="text" id="adress" name="adress" />
+        <br />
+        <label for="postalCode">Code postal :</label>
+        <input type="number" id="postalCode" name="postalCode" />
+        <br />
+        <label for="town">Ville :</label>
+        <input type="text" id="town" name="town" />
+        <br />
+        <label for="email">Adresse électronique :</label>
+        <input type="email" id="email" name="email" />
+        <br />
+        <input type="submit" value="Valider" />
+      </form>
+    </div>
+  </section>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Basket from '@/components/Basket.vue'
+
+// export default {
+//   name: 'Basket',
+//   components: {
+//     Basket
+//   }
+// }
+</script>
+
+<style>
+</style>
