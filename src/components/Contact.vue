2ece08dcfc21459515695ca8ff3f3e455d05856f
Céline Combe <celine.a.combe@gmail.com>
2020-07-05 16:46:40 +0200

    creation of the component 'Basket' and integration into the view 'Basket'

---
+++ src/components/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="form">
+    <form action class="contact">
+      Vos coordonnées
+      <br />
+      <label for="firstName">Prénom :</label>
+      <input type="text" id="firstName" name="firstName" />
+      <br />
+      <label for="lastName">Nom :</label>
+      <input type="text" id="lastName" name="lastName" />
+      <br />
+      <label for="adress">Adresse :</label>
+      <input type="text" id="adress" name="adress" />
+      <br />
+      <label for="postalCode">Code postal :</label>
+      <input type="number" id="postalCode" name="postalCode" />
+      <br />
+      <label for="town">Ville :</label>
+      <input type="text" id="town" name="town" />
+      <br />
+      <label for="email">Adresse électronique :</label>
+      <input type="email" id="email" name="email" />
+      <br />
+      <input type="submit" value="Valider" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Contact'
+}
+</script>
+
+<style>
+</style>
--- src/views/Basket.vue
+++ src/views/Basket.vue
@@ -1,52 +1,29 @@
 
 <template>
   <section class="basket">
-    <header>
-      <h1>Votre panier</h1>
-    </header>
+    <HeaderPage :headerClass="'basket'" :titre="'Votre panier'"></HeaderPage>
     <div class="main">
       <ul class="basket-contents">
         <li class="basket-article">Liste des articles du panier (à récupérer dans le component "x")</li>
       </ul>
     </div>
-    <div class="form">
-      <form action class="contact">
-        Vos coordonnées
-        <br />
-        <label for="firstName">Prénom :</label>
-        <input type="text" id="firstName" name="firstName" />
-        <br />
-        <label for="lastName">Nom :</label>
-        <input type="text" id="lastName" name="lastName" />
-        <br />
-        <label for="adress">Adresse :</label>
-        <input type="text" id="adress" name="adress" />
-        <br />
-        <label for="postalCode">Code postal :</label>
-        <input type="number" id="postalCode" name="postalCode" />
-        <br />
-        <label for="town">Ville :</label>
-        <input type="text" id="town" name="town" />
-        <br />
-        <label for="email">Adresse électronique :</label>
-        <input type="email" id="email" name="email" />
-        <br />
-        <input type="submit" value="Valider" />
-      </form>
-    </div>
+    <Contact></Contact>
   </section>
 </template>
 
 <script>
+import HeaderPage from '@/components/HeaderPage.vue'
+import Contact from '@/components/Contact.vue'
 // @ is an alias to /src
-// import Basket from '@/components/Basket.vue'
+// import BasketList from '@/components/BasketList.vue'
 
-// export default {
-//   name: 'Basket',
-//   components: {
-//     Basket
-//   }
-// }
+export default {
+  name: 'Basket',
+  components: {
+    Contact,
+    HeaderPage
+  }
+}
 </script>
 
 <style>
