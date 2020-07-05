51a1e3a9c8744a3788797e2abb76b16ec70d01bc
Céline Combe <celine.a.combe@gmail.com>
2020-07-05 16:54:55 +0200

    Deleting unnecessary files

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
 //import Basket from '../views/Basket.vue'
 
 Vue.use(VueRouter)
@@ -11,14 +10,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   },
   {
     path: '/basket',
