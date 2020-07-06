36c73eebae340b4f1a4041e3bb3eedbda7da8c96
Céline Combe <celine.a.combe@gmail.com>
2020-07-06 15:11:37 +0200

    Update components Product & ProductList

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,40 +1,43 @@
 <template>
   <div class="Product">
-    <ul class="liste-prod">
-      <li class="produit" v-for="teddy in teddies" :key="teddy._id">
-        <img class="teddyImage" :src="teddy.imageUrl" />
-        <p class="teddyName">Nom :{{teddy.name}}</p>
-        <!-- <p class="teddyDescription">Description:{{teddy.description}}</p> -->
-        <!-- <p class="teddyColors">Disponible dans les couleurs :{{teddy.colors}}</p> -->
-        <p class="teddyPrice">Prix: {{teddy.price}}</p>
-        <router-link :to="'/detail/'+teddy._id" tag="button">Voir cet article</router-link>
-        <br />
-      </li>
-    </ul>
+    <!-- <ul class="liste-prod">
+    <li class="produit" v-for="teddy in teddies" :key="teddy._id">-->
+    <img class="teddyImage" :src="teddy.imageUrl" />
+    <p class="teddyName">Nom :{{teddy.name}}</p>
+    <!-- <p class="teddyDescription">Description:{{teddy.description}}</p> -->
+    <!-- <p class="teddyColors">Disponible dans les couleurs :{{teddy.colors}}</p> -->
+    <p class="teddyPrice">Prix: {{teddy.price}}</p>
+    <router-link :to="'/detail/'+teddy._id" tag="button" :key="teddy._id">Voir cet article</router-link>
+    <br />
+    <!-- </li>
+    </ul>-->
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+// import axios from 'axios'
 export default {
 name: 'Product',
+props:{teddy:{
+  type: Object
+}},
 data () {
       return {
-          teddies: [],
-          loading: true,
-          errored: false
+    //       teddies: [],
+    //       loading: true,
+    //       errored: false
       }},
-      mounted () {
-    axios
-      .get('http://localhost:3000/api/teddies')
-      .then(response => {
-        this.teddies = response.data
-      })
-        .catch(error => {
-          console.log(error)
-          this.errored = true
-        })
-        .finally(() => this.loading = false)
+    //   mounted () {
+    // axios
+    //   .get('http://localhost:3000/api/teddies')
+    //   .then(response => {
+    //     this.teddies = response.data
+    //   })
+    //     .catch(error => {
+    //       console.log(error)
+    //       this.errored = true
+    //     })
+    //     .finally(() => this.loading = false)
 
     //   try {
     //       let response = await axios.get('http://localhost:3000/api/teddies')
@@ -43,7 +46,7 @@ data () {
     //     console.log(error)
     //   }
 
-  },
+  // },
   methods: {
       //goToDetail(): 
   }
--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,18 +1,41 @@
 <template>
   <section class="listProd">
     <div class="main">
-      <Product></Product>
+      <ul class="liste-prod">
+        <li class="produit" v-for="teddy in teddies" :key="teddy._id">
+          <Product :teddy="teddy"></Product>
+        </li>
+      </ul>
     </div>
   </section>
 </template>
 
 <script>
 import Product from '@/components/Product.vue'
+import axios from 'axios'
 export default {
 name: 'ProductList',
 components: {
   Product
-}
+},
+data () {
+      return {
+          teddies: [],
+          loading: true,
+          errored: false
+      }},
+      mounted () {
+    axios
+      .get('http://localhost:3000/api/teddies')
+      .then(response => {
+        this.teddies = response.data
+      })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
+      }
 }
 </script>
 
