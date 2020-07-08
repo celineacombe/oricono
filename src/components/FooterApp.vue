d835aab5c95b53d45e7d95438aa18566ab1f3e20
Céline Combe <celine.a.combe@gmail.com>
2020-07-08 17:00:32 +0200

    Update footerApp (position)

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,12 +1,14 @@
 <template>
-  <b-card body-bg-variant="light">
-    <b-row class="text-center">
-      <b-col>CVG</b-col>
-      <b-col>Mentions Légales</b-col>
-      <b-col>Politique de confidentialité</b-col>
-      <b-col>Contact</b-col>
-    </b-row>
-  </b-card>
+  <b-footer class="footer fixed-bottom">
+    <b-card body-bg-variant="light">
+      <b-row class="text-center">
+        <b-col>CVG</b-col>
+        <b-col>Mentions Légales</b-col>
+        <b-col>Politique de confidentialité</b-col>
+        <b-col>Contact</b-col>
+      </b-row>
+    </b-card>
+  </b-footer>
 </template>
 
 <script>
@@ -16,4 +18,9 @@ name: 'FooterApp'
 </script>
 
 <style>
-</style>
+.fixed-bottom {
+  right: 15px !important;
+  bottom: 15px !important;
+  left: 15px !important;
+}
+</style>>
