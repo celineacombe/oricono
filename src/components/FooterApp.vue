82621d538dee1fa04973aa160a0aad7af5fccd95
Céline Combe <celine.a.combe@gmail.com>
2020-07-08 11:58:05 +0200

    FooterApp style

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="main">
-    <footer class="footerApp">
-      <div class="cvg">CVG</div>
-      <div class="mentionsLegales">Mentions Légales</div>
-      <div class="PolConf">Politique de confidentialité</div>
-      <div class="contact">Contact</div>
-    </footer>
-  </div>
+  <b-card title="Card Title" no-body>
+    <b-card-header header-tag="nav">
+      <b-nav card-header pills fill>
+        <b-nav-item disabled>CVG</b-nav-item>
+        <b-nav-item disabled>Mentions Légales</b-nav-item>
+        <b-nav-item disabled>Politique de confidentialité</b-nav-item>
+        <b-nav-item disabled>Contact</b-nav-item>
+      </b-nav>
+    </b-card-header>
+  </b-card>
 </template>
 
 <script>
@@ -16,10 +18,4 @@ name: 'FooterApp'
 </script>
 
 <style>
-.footerApp {
-  background-color: lightgray;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-}
 </style>
