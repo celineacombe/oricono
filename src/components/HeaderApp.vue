7b9b79ac4dbfe2a4605dd57e8d8100d4fadb7830
Céline Combe <celine.a.combe@gmail.com>
2020-07-08 11:25:08 +0200

    HeaderApp style

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,8 +1,14 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>|
-    <router-link to="/basket">Panier</router-link>
-  </div>
+  <b-navbar toggleable="lg" variant="primary">
+    <b-navbar-brand :to="'/'">Orinoco</b-navbar-brand>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav id="nav">
+        <b-nav-item :to="'/'">Home</b-nav-item>
+        <b-nav-item :to="'/basket'">Panier</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <script>
