b95d907caa3e358793db17a88de8dedf04831fd2
Céline Combe <celine.a.combe@gmail.com>
2020-07-10 11:03:31 +0200

    Contact.vue style

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,6 +1,69 @@
 <template>
-  <div class="form">
-    <form action class="contact">
+  <b-form-row class="justify-content-md-center">
+    <b-card bg-variant="light" class="col-md-6">
+      <b-form-group
+        label-cols-lg="3"
+        label="Vos coordonnées"
+        label-size="lg"
+        label-class="font-weight-bold pt-0"
+        class="mb-0"
+      >
+        <b-form-group
+          label-cols-sm="3"
+          label-cols-lg="2"
+          label="Nom :"
+          label-align-sm="right"
+          label-for="nested-lastName"
+        >
+          <b-form-input id="lastName" v-model="contact.lastName" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          label-cols-sm="3"
+          label-cols-lg="2"
+          label="Prénom :"
+          label-align-sm="right"
+          label-for="nested-firstName"
+        >
+          <b-form-input id="firstName" v-model="contact.firstName" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          label-cols-sm="3"
+          label-cols-lg="2"
+          label="Adresse :"
+          label-align-sm="right"
+          label-for="nested-address"
+        >
+          <b-form-input id="address" v-model="contact.address" required></b-form-input>
+        </b-form-group>
+        <b-form-group
+          label-cols-sm="3"
+          label-cols-lg="2"
+          label="Ville :"
+          label-align-sm="right"
+          label-for="nested-city"
+        >
+          <b-form-input id="city" v-model="contact.city" required></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+          label-cols-sm="3"
+          label-cols-lg="2"
+          label="Email :"
+          label-align-sm="right"
+          label-for="nested-email"
+        >
+          <b-form-input id="email" v-model="contact.email" required></b-form-input>
+        </b-form-group>
+        <b-form-group label-cols-sm="3" label-cols-lg="2" label-align-sm="right">
+          <b-button type="submit" variant="primary" @click="checkoutOrder">Valider la commande</b-button>
+        </b-form-group>
+      </b-form-group>
+    </b-card>
+  </b-form-row>
+
+  <!-- <form action class="contact">
       Vos coordonnées
       <br />
       <label for="firstName">Prénom :</label>
@@ -19,8 +82,7 @@
       <input type="email" id="email" name="email" v-model="contact.email" required />
       <br />
       <button class @click="checkoutOrder">Valider la commande</button>
-    </form>
-  </div>
+  </form>-->
 </template>
 
 <script>
